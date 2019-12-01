38802b82222b0154a47c52053b4948e185dec75a
NIKALyb <58136351+NIKALyb@users.noreply.github.com>
2019-12-01 14:11:56 +0300

    Curse v2.0 without error

--- curse.py
+++ curse.py
@@ -5,7 +5,7 @@ import mcpi.block as block
 import time
 #variable
 a = 0
-#infinite loop
+#loop
 while a < 20:
     pos = mc.player.getTilePos()
     x = round(pos.x)
@@ -14,4 +14,4 @@ while a < 20:
     mc.setBlocks(x, y, z, x, y+1, z, 8)
     a += 1
     time.sleep(0.5)
-    	mc.postToChat("You death soon")
+    mc.postToChat("You death soon")
